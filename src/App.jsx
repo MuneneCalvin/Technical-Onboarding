56630e2e2dd1811f407bd8046eb83b943cdc63ad
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2023-09-13 11:15:47 +0000

    Feat: updated about me

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Calvin Mwangi",
+  title: "Software Engineer",
+  email: "munenecalvn@gmail.com",
+  gitHub: "MuneneCalvin",
+  instagram: "__future.west__",
+  linkedIn: "calvin-munene",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
