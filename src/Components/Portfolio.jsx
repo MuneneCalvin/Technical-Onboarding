7231d1bfa13fa968cadb82f868a14a377774b96f
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2023-09-13 11:32:49 +0000

    feat: added pictures

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/code-1076536_1280.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Code on a computer screen";
 
 /**
  * Project list
