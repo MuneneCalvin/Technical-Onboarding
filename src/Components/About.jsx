166148fa47ced837de79a50fc711aa028c97160d
Calvin Munene <96726493+MuneneCalvin@users.noreply.github.com>
2023-09-14 06:37:09 +0000

    feat: updated pics

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/karl-unsplash.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "A person working on a computer";
 
 /**
  * Sort description that expands on your title on the Home component.
